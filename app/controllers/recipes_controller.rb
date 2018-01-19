bfc83f9275f0b8ca7e5ce447ddc3c029ab261288
nathanknautz <nate.knautz@gmail.com>
2018-01-18 19:53:04 -0600

    updated with restful

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,23 @@
 class RecipesController < ApplicationController
-  def one_recipe_method
-    recipe = Recipe.first
-    render json: {title: recipe.title,
-                  chef: recipe.chef,
-                  ingredients: recipe.ingredients,
-                  directions: recipe.directions
-                 }
-              
+  def index
+    recipes = Recipe.all 
+
+    render json: recipes.as_json   
   end
+
+  def create
+    recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions])
+    recipe.save 
+    render json: recipe.as_json 
+  end
+
+  def show
+    recipe = Recipe.find(params[:id])
+    render json: recipe.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get "/one_recipe_url" => 'recipes#one_recipe_method'
+  get "/recipes" => 'recipes#index'
+  post '/recipes' => 'recipes#create'
+  get '/recipes/:id' => 'recipes#show'
 end
